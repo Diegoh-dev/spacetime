d32103e5a06ffc0a81500700a9bbbc87289d65f5
Diegoh-dev <diegoh.developer@gmail.com>
2023-05-20 00:04:32 -0300

    componentizando a aplicação web.

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export default function EmptyMemories(){
+  return (
+    <div className="flex flex-1 items-center justify-center">
+          <p className=" w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{""}{" "}
+            <a target="_blank" rel="noreferrer" href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>{" "}
+            {/* apenas paa subir a versão */}
+          </p>
+        </div>
+  )
+}
